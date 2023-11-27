91d21665a8f90a31e6b324a4e2970587b6617bc9
Ashutosh Motla <aashutoshmotla3@gmail.com>
2023-11-27 11:55:59 +0530

    login

---
+++ rineapp/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+from django_rest_passwordreset.signals import reset_password_token_created
+# Create your models here.
---
+++ rineapp/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Create your views here.
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from . models import *
+from django.contrib.auth import authenticate, login, logout
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get('usernumber')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        if len(username) > 10 or len(username) < 10:
+            messages.info(request, "Phone Number Must be 10 Digits")
+            return redirect('/signup')
+
+        if pass1 != pass2:
+            messages.info(request, "Password is not Matching")
+            return redirect('/signup')
+
+        try:
+            if User.objects.get(username=username):
+                messages.warning(request, "Phone Number is Taken")
+                return redirect('/signup')
+
+        except Exception as identifier:
+            pass
+
+        try:
+            if User.objects.get(email=email):
+                messages.warning(request, "Email is Taken")
+                return redirect('/signup')
+
+        except Exception as identifier:
+            pass
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.save()
+        messages.success(request, "User is Created Please Login")
+        return redirect('/login')
+
+    return render(request, "signup.html")
+
+
+def handlelogin(request):
+    if request.method == "POST":
+        username = request.POST.get('usernumber')
+        pass1 = request.POST.get('pass1')
+        myuser = authenticate(username=username, password=pass1)
+        if myuser is not None:
+            login(request, myuser)
+            messages.success(request, "Login Successful")
+            return redirect('/home')
+        else:
+            messages.error(request, "Invalid Credentials")
+            return redirect('/login')
+
+    return render(request, "login.html")
+
+
+def handleLogout(request):
+    logout(request)
+    messages.success(request, "Logout Success")
+    return redirect('/login')
+
+
+def home(request):
+    return render(request, "home.html")
